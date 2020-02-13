d0fc6013258397b905f992af5fa8a951fdc3e105
xingweitian <13183370+xingweitian@users.noreply.github.com>
2020-02-13 15:26:38 -0500

    Update test cases.

--- testing/Unsat.java
+++ testing/Unsat.java
@@ -5,6 +5,7 @@ public class Unsat {
 
   	void inferTop(@TopSecret String p) {
         String s = p;
+        // :: fixable-error: (assignment.type.incompatible)
         @Public String x = s;
     }
 }
