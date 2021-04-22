261726e4114b11d2f24914a2ccb56220681b7804
d367wang <55197967+d367wang@users.noreply.github.com>
2021-04-22 14:48:55 -0400

    Skip constructor result check (#6)

--- src/security/SecurityVisitor.java
+++ src/security/SecurityVisitor.java
@@ -3,6 +3,9 @@ package security;
 import checkers.inference.InferenceChecker;
 import checkers.inference.InferenceVisitor;
 import org.checkerframework.common.basetype.BaseAnnotatedTypeFactory;
+import org.checkerframework.framework.type.AnnotatedTypeMirror;
+
+import javax.lang.model.element.ExecutableElement;
 
 public class SecurityVisitor extends InferenceVisitor<SecurityChecker, BaseAnnotatedTypeFactory> {
     public SecurityVisitor(
@@ -12,4 +15,12 @@ public class SecurityVisitor extends InferenceVisitor<SecurityChecker, BaseAnnot
             boolean infer) {
         super(checker, ichecker, factory, infer);
     }
+
+    /**
+     * Don't check that the constructor result is top. In the security type system, it is the expected
+     * behavior that an object is created to be public.
+     */
+    @Override
+    protected void checkConstructorResult(
+            AnnotatedTypeMirror.AnnotatedExecutableType constructorType, ExecutableElement constructorElement) {}
 }
